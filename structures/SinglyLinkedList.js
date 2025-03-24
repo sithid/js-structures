9a60f785261a688295deda98f044dafbd2b5c4ce
sithid <demonicurges05@gmail.com>
2025-03-24 04:51:09 -0400

    SinglyLinkedList
    Basic singly linked list implemented. count, push(data), pop(), peek(). This linked list inserts and at and pops from the head (LIFO).

--- structures/SinglyLinkedList.js
+++ structures/SinglyLinkedList.js
@@ -10,13 +10,48 @@ class SinglyLinkedList {
     this.head = null;
     this.count = 0;
   }
+
+  // Insert a new node at the head of the linked list.
   push(data) {
     let newHead = new SinglyLinkedListNode(data);
+
+    if (this.head === null) {
+      this.head = newHead;
+      ++this.count;
+      return;
+    }
+
+    newHead.next = this.head;
+    this.head = newHead;
+    ++this.count;
   }
 
-  pop(data) {}
+  pop() {
+    if (this.count < 1) {
+      return null;
+    } else {
+      let data = this.head.data;
 
-  peek(index) {}
+      if (this.count > 1) {
+        this.head = this.head.next;
+        --this.count;
+
+        return data;
+      } else {
+        this.head = null;
+        --this.count;
+        return data;
+      }
+    }
+  }
+
+  peek() {
+    if (this.head === null) {
+      return null;
+    }
+
+    return this.head.data;
+  }
 }
 
-module.exports = { LinkedList };
+module.exports = { SinglyLinkedList };
